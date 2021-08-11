d68e4ef139e810407b6882716c105a375e4f01ed
CZJoanna <jyunchen199512@gmail.com>
2021-08-11 15:33:28 +0800

    propject initialize

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="todoapp">
+    <header>
+      <h1>todos</h1>
+    </header>
+    <input type="text" class="new-todo" autofocus placeholder="需要做什麼？" />
+    <main>
+      <input id="toggle-all" type="checkbox" class="toggle-all" />
+      <label for="toggle-all"></label>
+      <ul class="todo-list">
+        <li class="todo">
+          <div class="view">
+            <input type="checkbox" class="toggle" />
+            <label for="">學習 Vue Template</label>
+            <button class="destroy"></button>
+          </div>
+          <input type="text" class="edit" />
+        </li>
+        <li class="todo">
+          <div class="view">
+            <input type="checkbox" class="toggle" />
+            <label for="">學習在 Vue Template 中進行條件判斷</label>
+            <button class="destroy"></button>
+          </div>
+          <input type="text" class="edit" />
+        </li>
+        <li class="todo">
+          <div class="view">
+            <input type="checkbox" class="toggle" />
+            <label for="">學習在 Vue Template 中使用迴圈 </label>
+            <button class="destroy"></button>
+          </div>
+          <input type="text" class="edit" />
+        </li>
+      </ul>
+    </main>
+    <footer class="footer">
+      <span class="todo-count"> <strong> 3 </strong> items left </span>
+      <ul class="filters">
+        <li>
+          <a href="#/all">All</a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+        </li>
+      </ul>
+      <button class="clear-completed">Clear completed</button>
+    </footer>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
