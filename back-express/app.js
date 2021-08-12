438649812652d7b84b1048d296268f44b5ff3b17
CZJoanna <jyunchen199512@gmail.com>
2021-08-12 23:48:05 +0800

    feat: add mysql and fetch todo function

--- back-express/app.js
+++ back-express/app.js
@@ -1,10 +1,60 @@
 // 載入 express 並架起伺服器
 const express = require("express");
+// 引用 body-parser -> ! 請直接用 express 替代 !
+// const bodyParser = require("body-parser");
 const app = express();
+
 //設定 port 號 3000
 const port = 3000;
+
+// 引入cors 並預設為全開放
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 載入 mysql
+const mysql = require("mysql");
+// 連線物件
+const conn = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "jyun",
+  password: "1212",
+  database: "todo-list",
+  connectionLimit: 10,
+});
+// 連線資料庫
+conn.connect(function (err) {
+  if (err) {
+    console.log(JSON.stringify(err));
+    return;
+  } else {
+    console.log("已連接到mysql");
+  }
+});
 
 // 伺服器啟動後開始監聽這個服務窗口，有從這個窗口進來的瀏覽器請求，就會被受理～
 app.listen(port, () => {
   console.log("App is running ");
+});
+
+//讀取全部資料
+app.get("/", (req, res) => {
+  conn.query("select * from todo", "", function (err, rows) {
+    if (err) {
+      console.log(JSON.stringify(err));
+      return;
+    } else {
+      res.send(JSON.stringify(rows));
+    }
+  });
+});
+
+//新增一筆資料
+app.post("/", (req, res) => {
+  //瀏覽器送過來要進入資料庫的物件
+  console.log(req.body); //{...}
+  res.send("收到了");
+  // conn.query("insert into todo",)
 });
